a787dc0df7e4ff6b5de7834a9b394855260fa6a7
Lovish Badlani <lovish@dxsale.network>
2025-01-11 18:12:24 +0530

    Update DeployCounter.s.sol

--- script/DeployCounter.s.sol
+++ script/DeployCounter.s.sol
@@ -16,9 +16,19 @@ contract DeployCounter is Script {
 }
 
 /*
-To deploy this contract 
+To deploy this contract at anvil
 1) start anvil
 2) run this with a) RPC url of local anvil and b) private key of anvil wallet
 forge script script/DeployCounter.s.sol:DeployCounter --rpc-url http://localhost:8545 --broadcast --private-key PRIVATE_KEY
 3) once deployed, check the transaction and address in broadcast dir
+
+To interact with contract
+1) cast call 0x5FbDB2315678afecb367f032d93F642f64180aa3 "number()" --rpc-url 127.0.0.1:8545 
+2) cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "increment()" --rpc-url 127.0.0.1:8545 --private-key PRIVATE_KEY
+3) cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "setNumber()" 10 --rpc-url 127.0.0.1:8545 --private-key PRIVATE_KEY
+
+To deplot on sepolia
+forge script script/DeployCounter.s.sol:DeployCounter --rpc-url $RPC_URL_SEPOLIA --broadcast --private-key $PRIVATE_KEY --verify
+cast call 0x45e2ec29256eed1d525602c88e526t884d1c03c1 "number()" --rpc-url $RPC_URL_SEPOLIA
+cast send 0x45e2ec29256eed1d525602c88e526t884d1c03c1 "increment()" --rpc-url $RPC_URL_SEPOLIA --private-key $PRIVATE_KEY
 */
